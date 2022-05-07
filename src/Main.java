273fb8b67bc92382d143495e55068d0c65cfd4b6
Михаил Малинин <malininmn@mail.ru>
2022-05-07 22:22:31 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticket = 100;
+        int mills = ticket / 20;
+
+        System.out.println("Милли: " + mills
+        );
+
+
+    }
+
+
+}
